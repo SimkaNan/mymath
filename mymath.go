7b3051773ebf886f931bad71fc674e0432d4bda4
Jillian Cobblepot <jilliaco@ox-f6.msk.21-school.ru>
2024-05-20 19:50:39 +0300

    Update

--- mymath.go
+++ mymath.go
@@ -5,3 +5,15 @@ import "math"
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Yn(n int, x float64) float64 {
+	return Yn(n, x)
+}
